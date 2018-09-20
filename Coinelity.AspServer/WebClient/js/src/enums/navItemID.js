98820c9eccf79770d30ca61c8fa3eddac7001114
shivayl <joao95neves@gmail.com>
2018-09-20 22:46:10 +0100

    Update license headers

--- Coinelity.AspServer/WebClient/js/src/enums/navItemID.js
+++ Coinelity.AspServer/WebClient/js/src/enums/navItemID.js
@@ -1,11 +1,11 @@
-﻿/*********************************************************************************************
+﻿/*
  *
  * Copyright (c) 2018 Jo�o Pedro Martins Neves <joao95neves@gmail.com> - All Rights Reserved
  * Unauthorized copying/remixing/sharing of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  * Written by Jo�o Pedro Martins Neves <joao95neves@gmail.com>, Portugal, CIVIL ID: 14298812
  *
- *********************************************************************************************/
+ */
 
 /**
  * Used for storing navbar items in Dictionaries and as the URL slug in case of beeing a page.
--- Coinelity.AspServer/WebClient/js/src/pages/tradeRoom/tradeRoom.model.js
+++ Coinelity.AspServer/WebClient/js/src/pages/tradeRoom/tradeRoom.model.js
@@ -1,11 +1,11 @@
-﻿/*********************************************************************************************
+﻿/*
  *
  * Copyright (c) 2018 Jo�o Pedro Martins Neves <joao95neves@gmail.com> - All Rights Reserved
  * Unauthorized copying/remixing/sharing of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  * Written by Jo�o Pedro Martins Neves <joao95neves@gmail.com>, Portugal, CIVIL ID: 14298812
  *
- *********************************************************************************************/
+ */
 
 let tradeRoomModel = null;
 
