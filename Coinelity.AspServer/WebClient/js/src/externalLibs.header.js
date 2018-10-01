67dd50fa4dbe47cbdf13882125b2f5cd170aa191
shivayl <joao95neves@gmail.com>
2018-10-01 22:26:17 +0100

    External libs
    - Separated the external libraries from the frontend Coinelity core;
    - Installed plotly.js (financial) via npm. Preparing to migrate from highstock to plotly.

---
+++ Coinelity.AspServer/WebClient/js/src/externalLibs.header.js
@@ -0,0 +1,8 @@
+﻿// @import '/services/externalLibs'
+// $import 'when-dom-ready/dist/index.umd'
+// $import 'sweetalert2/dist/sweetalert2.all.min'
+// $import 'ccxt/build/ccxt.browser'
+// $import '@aspnet/signalr/dist/browser/signalr.min'
+// $import 'plotly.js-dist/plotly'
+// $import 'plotly.js-finance-dist/plotly-finance'
+// 'highcharts/highstock' // NOT IMPORTING
