c5f0cbb3f875ec0a07a5fdbebb9463ed9dedd6f3
João Neves <joao95neves@gmail.com>
2019-05-05 20:59:25 +0100

    Update Coinelity (Frontend)
    Continued working on the outer app (header + aside + pages).

--- Coinelity.AspServer/WebApp/src/app/app.module.ts
+++ Coinelity.AspServer/WebApp/src/app/app.module.ts
@@ -4,33 +4,33 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { AsideComponent } from './components/aside/aside.component';
-import { SidenavPanelComponent } from './components/aside/sidenav-panel/sidenav-panel.component';
-import { ChatComponent } from './components/aside/sidenav-panel/chat/chat.component';
-import { NewsComponent } from './components/aside/sidenav-panel/news/news.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { SidenavPanelComponent } from './components/sidenav/sidenav-panel/sidenav-panel.component';
+import { ChatComponent } from './components/sidenav/sidenav-panel/chat/chat.component';
+import { NewsComponent } from './components/sidenav/sidenav-panel/news/news.component';
 import { PagesComponent } from './components/pages/pages.component';
 import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
 import { TradeRoomComponent } from './components/pages/trade-room/trade-room.component';
-import { TradeComponent } from './components/pages/trade-room/trade/trade.component';
 import { MarketsComponent } from './components/pages/trade-room/markets/markets.component';
+import { TradeComponent } from './components/pages/trade-room/trade/trade.component';
 import { SettingsComponent } from './components/pages/settings/settings.component';
 import { AffiliateComponent } from './components/pages/affiliate/affiliate.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AsideComponent,
+    SidenavComponent,
+    SidenavPanelComponent,
+    ChatComponent,
+    NewsComponent,
     HeaderComponent,
     PagesComponent,
     DashboardComponent,
-    SettingsComponent,
     TradeRoomComponent,
-    AffiliateComponent,
-    TradeComponent,
     MarketsComponent,
-    SidenavPanelComponent,
-    ChatComponent,
-    NewsComponent
+    TradeComponent,
+    SettingsComponent,
+    AffiliateComponent
   ],
   imports: [
     BrowserModule,
---
+++ Coinelity.AspServer/WebApp/src/app/components/sidenav/sidenav-panel/sidenav-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav-panel',
+  templateUrl: './sidenav-panel.component.html',
+  styleUrls: ['./sidenav-panel.component.scss']
+})
+export class SidenavPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
